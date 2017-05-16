8b0fe23ed8c30c1d78f01e5a633653031ea4476e
cnlohr <lohr85@gmail.com>
2017-05-16 03:03:46 -0400

    bump submodule

--- firmware/user/user_main.c
+++ firmware/user/user_main.c
@@ -30,17 +30,6 @@ void user_rf_pre_init(void)
 }
 
 
-
-char * strcat( char * dest, char * src )
-{
-    char *rdest = dest;
-    while (*dest) dest++;
-    while (*dest++ = *src++);
-    return rdest;
-}
-
-
-
 //Tasks that happen all the time.
 
 os_event_t    procTaskQueue[procTaskQueueLen];
@@ -172,35 +161,3 @@ void ExitCritical()
 }
 
 
-//For SDK 2.0.0 only.
-uint32 ICACHE_FLASH_ATTR user_rf_cal_sector_set(void)
-{
-    enum flash_size_map size_map = system_get_flash_size_map();
-    uint32 rf_cal_sec = 0;
-
-    switch (size_map) {
-        case FLASH_SIZE_4M_MAP_256_256:
-            rf_cal_sec = 128 - 8;
-            break;
-
-        case FLASH_SIZE_8M_MAP_512_512:
-            rf_cal_sec = 256 - 5;
-            break;
-
-        case FLASH_SIZE_16M_MAP_512_512:
-        case FLASH_SIZE_16M_MAP_1024_1024:
-            rf_cal_sec = 512 - 5;
-            break;
-
-        case FLASH_SIZE_32M_MAP_512_512:
-        case FLASH_SIZE_32M_MAP_1024_1024:
-            rf_cal_sec = 1024 - 5;
-            break;
-
-        default:
-            rf_cal_sec = 0;
-            break;
-    }
-
-    return rf_cal_sec;
-}
